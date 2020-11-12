35a0fd3d482e40d123bafe2182f2629e40c48089
Boris Lebedev <lebedev.moscow.me@gmail.com>
2020-11-12 07:07:36 +0500

    Continue to learning

--- src/Toggle.js
+++ src/Toggle.js
@@ -2,17 +2,27 @@ import React, { useState } from 'react'
 import { useSpring, animated } from 'react-spring'
 
 const Toggle = () => {
-    const [isToggled, setToggle] = useState(false)
-    const fade = useSpring({
-        opacity: isToggled ? 1 : 0,
-    })
+	const [isToggled, setToggle] = useState(false)
+	const { color, y } = useSpring({
+		color: isToggled ? 'tomato' : 'green',
+		y: isToggled ? 0 : -50,
+	})
 
-    return (
-        <div>
-            <animated.h1 style={fade}>Hello</animated.h1>
-            <button onClick={() => setToggle(!isToggled)}>Toggle</button>
-        </div>
-    )
+	return (
+		<div>
+			<animated.h1
+				style={{
+					transform: y.interpolate(
+						(y) => `translate3d(0, ${y}px, 0)`
+					),
+					color,
+				}}
+			>
+				Hello
+			</animated.h1>
+			<button onClick={() => setToggle(!isToggled)}>Toggle</button>
+		</div>
+	)
 }
 
 export default Toggle
