6b3c0d074fc04e90095d183b2348fdf9c209a244
Boris Lebedev <lebedev.moscow.me@gmail.com>
2020-11-12 07:24:58 +0500

    Add animation to nav menu (8)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSpring, animated } from 'react-spring'
 import Toggle from './Toggle'
+import Nav from './Nav'
 import logo from './logo.svg'
 import './App.css'
 
 const App = () => {
-    const fade = useSpring({
-        from: {
-            opacity: 0,
-        },
-        opacity: 1,
-    })
+	const [isNavOpen, setNavOpen] = useState(false)
+	const navAnimation = useSpring({
+		transform: isNavOpen
+			? 'translate3d(0,0,0) scale(1)'
+			: 'translate3d(100%,0,0) scale(0)',
+	})
 
-    return (
-        <animated.div className="App" style={fade}>
-            <header className="App-header">
-                <img src={logo} className="logo"></img>
-                <button className="menu-button">Menu</button>
-            </header>
-            <main>
-                <Toggle />
-            </main>
-        </animated.div>
-    )
+	const fade = useSpring({
+		from: {
+			opacity: 0,
+		},
+		opacity: 1,
+	})
+
+	return (
+		<animated.div className='App' style={fade}>
+			<header className='App-header'>
+				<img src={logo} className='logo'></img>
+				<button
+					onClick={() => setNavOpen(!isNavOpen)}
+					className='menu-button'
+				>
+					Menu
+				</button>
+			</header>
+			<main>
+				<Toggle />
+				<Nav style={navAnimation} />
+			</main>
+		</animated.div>
+	)
 }
 
 export default App
