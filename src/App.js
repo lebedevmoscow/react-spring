ce7871cb19dd8c4cdc979bbc9113eca332077852
Boris Lebedev <lebedev.moscow.me@gmail.com>
2020-11-15 02:54:35 +0500

    Add drag and drop feature (16)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Checkout from './Checkout'
 import Modal from './Modal'
 import Accordion from './Accordion'
 import Waypoints from './Waypoints'
+import Gesture from './Gesture'
 import logo from './logo.svg'
 import './App.css'
 
@@ -38,7 +39,8 @@ const App = () => {
 			<main>
 				<Modal />
 				<Accordion />
-				<Waypoints />
+				{/* <Waypoints /> */}
+				<Gesture />
 			</main>
 			<Checkout isOpen={isNavOpen} />
 		</animated.div>
