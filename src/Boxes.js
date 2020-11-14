5f8f7bdd0d00b55a24297d034fab9c8327ff7b2e
Boris Lebedev <lebedev.moscow.me@gmail.com>
2020-11-15 03:23:46 +0500

    Get acquainted with useHook (19)

--- src/Boxes.js
+++ src/Boxes.js
@@ -1,24 +1,21 @@
 import React from 'react'
-import { useSprings, animated } from 'react-spring'
+import { useTrail, animated } from 'react-spring'
 
 const items = [0, 1, 2, 3, 4]
 
 const Boxes = () => {
-	const springs = useSprings(
-		items.length,
-		items.map((item) => ({
-			from: {
-				opacity: 0,
-			},
-			to: {
-				opacity: 1,
-			},
-		}))
-	)
+	const trail = useTrail(items.length, {
+		from: {
+			opacity: 0,
+		},
+		to: {
+			opacity: 1,
+		},
+	})
 
 	return (
 		<div className='boxes-grid'>
-			{springs.map((animation) => (
+			{trail.map((animation) => (
 				<animated.div style={animation} className='box'></animated.div>
 			))}
 		</div>
